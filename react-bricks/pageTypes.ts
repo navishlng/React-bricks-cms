c302acfe9844b22f44fa09d84ff4fad25685cce5
Dario Ronzoni <dario.ronzoni@f2.net>
2024-01-16 17:25:06 +0100

    feat: updated pageTypes

--- react-bricks/pageTypes.ts
+++ react-bricks/pageTypes.ts
@@ -8,27 +8,15 @@ const pageTypes: types.IPageType[] = [
     defaultLocked: false,
     defaultStatus: types.PageStatus.Published,
     getDefaultContent: () => [],
-    // customFields: [
-    //   {
-    //     name: 'test',
-    //     type: types.SideEditPropType.Text,
-    //     label: 'Test',
-    //   },
-    // ],
+    excludedBlockTypes: ['pokemon'],
   },
+
   {
     name: 'blog',
     pluralName: 'Blog',
     defaultLocked: false,
     defaultStatus: types.PageStatus.Published,
     getDefaultContent: () => [],
-    slugPrefix: {
-      default: 'news/',
-      translations: {
-        en: 'news/',
-        it: 'novitax/',
-      },
-    },
     allowedBlockTypes: [
       'title',
       'paragraph',
@@ -39,63 +27,8 @@ const pageTypes: types.IPageType[] = [
       'tweet-light',
       'blog-title',
       'newsletter-subscribe',
-      'external-data-example',
-      'author-card',
-      'authors-list',
     ],
-    getExternalData: () =>
-      fetch('https://catfact.ninja/fact')
-        .then((response) => response.json())
-        .then((data) => ({
-          catFact: data.fact,
-        })),
-    customFields: [
-      {
-        name: 'authors',
-        label: 'Authors',
-        type: types.SideEditPropType.Relationship,
-        relationshipOptions: {
-          references: 'author',
-          label: 'Author',
-          multiple: true,
-          embedValues: true,
-        },
-      },
-    ],
-  },
-
-  {
-    name: 'layout',
-    pluralName: 'layout',
-    defaultLocked: false,
-    defaultStatus: types.PageStatus.Published,
-    getDefaultContent: () => [],
-    isEntity: true,
-  },
-
-  {
-    name: 'author',
-    isEntity: true,
-    pluralName: 'authors',
-    defaultLocked: false,
-    defaultStatus: types.PageStatus.Published,
-    customFields: [
-      {
-        name: 'firstName',
-        label: 'First Name',
-        type: types.SideEditPropType.Text,
-      },
-      {
-        name: 'lastName',
-        label: 'Last Name',
-        type: types.SideEditPropType.Text,
-      },
-      {
-        name: 'photo',
-        label: 'Photo',
-        type: types.SideEditPropType.Image,
-      },
-    ],
+    excludedBlockTypes: ['pokemon'],
   },
   {
     name: 'pokemon',
@@ -106,46 +39,20 @@ const pageTypes: types.IPageType[] = [
         .then((data) => ({
           ...data,
           imageUrl: `https://img.pokemondb.net/artwork/large/${data.name}.jpg`,
-        })),
-    // getExternalData: (_, args) => {
-    //   console.log('args', args);
-    //   return fetch(
-    //       `https://acc-cortina-api.azurewebsites.net/api/v1/dealers/${
-    //           args?.dealerId || '100438'
-    //       }`
-    //   )
-    //       .then((response) => response.json())
-    //       .then((data) => ({
-    //           data,
-    //       }));
+        }))
+        .catch((error) => {
+          console.log(error)
+          return {}
+        }),
   },
   {
-    name: 'pokemon-list',
-    pluralName: 'pokemon lists',
-    getExternalData: async (page, args) => {
-      // const tagArgs = args?.tag ? { tag: args.tag } : {}
-      // const pages = await fetchPages(config.apiKey, {
-      //   ...tagArgs,
-      //   type: 'blog',
-      //   pageSize: 4,
-      //   sort: '-publishedAt',
-      // })
-
-      // const tags = await fetchTags(config.apiKey)
-
-      // return {
-      //   pagesByTag: pages,
-      //   allTags: tags?.items || [],
-      // }
-
-      return fetchPages({
-        type: 'pokemon',
-        fetchExternalData: true,
-        config,
-      }).then((pokemons) => ({
-        pokemons,
-      }))
-    },
+    name: 'layout',
+    pluralName: 'layout',
+    defaultLocked: false,
+    defaultStatus: types.PageStatus.Published,
+    getDefaultContent: () => [],
+    isEntity: true,
+    allowedBlockTypes: ['header', 'footer'],
   },
 ]
 
